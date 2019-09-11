16f6b6adf1b1890719981c3f400985674969e039
Mariana Templin <mariana.acuna.templin@gmail.com>
2019-09-11 13:20:38 -0400

    arrow and space bar nav added

--- src/slideshow.js
+++ src/slideshow.js
@@ -2,51 +2,13 @@ var slideIndex = 0;
 var slides = data.slides
 var slideshowContainer = document.getElementById("slideshow-container");
 
-// createSlide(slideIndex);
-
-// function createSlide(n) {
-
-//   var slides = data.slides
-//   var slideshowContainer = document.getElementById("slideshow-container");
-
-//   if (n > slides.length - 1) {slideIndex = 0}
-//   if (n < 0) {slideIndex = slides.length - 1}
-
-//   var currentSlide = slides[slideIndex]
-
-//   let slideDiv = document.createElement('div');
-//   slideDiv.setAttribute('class', 'mySlides fade')
-
-//   let slideImg
-//   let slideText
-//   if(!currentSlide.image) {
-//     slideImg = document.createElement('div');
-//     slideImg.innerText = currentSlide.text.replace('\n', '<br />')
-//   } else {
-//     slideImg = document.createElement('img');
-//     slideImg.setAttribute('src', data.path + currentSlide.image)
-//   }
-
-//   slideDiv.appendChild(slideImg);
-
-//   slideText = document.createElement('div');
-//   slideText.setAttribute('class', 'text');
-//   slideText.innerText = currentSlide.text.replace('\n', '\r\n')
-//   slideDiv.appendChild(slideText)
-
-//   var textHeight = currentSlide.text.split('\n').length * 75
-
-//   slideImg.style.height = 'calc(100% - ' + textHeight + 'px)'
-
-//   var replaceSlide = slideshowContainer.querySelector('div')
-
-//   if(!replaceSlide) {
-//     slideshowContainer.appendChild(slideDiv)
-//   } else {
-//     slideshowContainer.replaceChild(slideDiv, replaceSlide)
-//   }
-
-// }
+window.addEventListener("keydown", function(event) {
+  if (event.key==="ArrowRight" || event.key===" "){
+    plusSlides(1)
+  } else if (event.key==="ArrowLeft"){
+    plusSlides(-1)
+  }
+}, true);
 
 function plusSlides(n) {
   displaySlide(slideIndex += n);
@@ -57,27 +19,25 @@ function createSlides() {
     let slideDiv = document.createElement('div');
     slideDiv.setAttribute('class', 'mySlides fade')
 
-    let slideImg
-    let slideText
+    // let slideImg
+    let slideText = document.createElement('div');
+    slideText.setAttribute('class', 'text');
+    slideText.innerText = slide.text.replace('\n', '\r\n')
+    //append 2 divs
     if(!slide.image) {
-      slideImg = document.createElement('div');
-      slideImg.innerText = slide.text.replace('\n', '<br />')
+
+      slideDiv.setAttribute('class', 'mySlides fade blank-slide')
     } else {
       slideImg = document.createElement('img');
       slideImg.setAttribute('src', data.path + slide.image)
+
+      var textHeight = (slide.text.split('\n').length - 1) * 60 + 75
+      slideImg.style.height = 'calc(100% - ' + textHeight + 'px)'
+
+      slideDiv.appendChild(slideImg);
     }
 
-    slideDiv.appendChild(slideImg);
-
-    slideText = document.createElement('div');
-    slideText.setAttribute('class', 'text');
-    slideText.innerText = slide.text.replace('\n', '\r\n')
     slideDiv.appendChild(slideText)
-
-    var textHeight = slide.text.split('\n').length * 75
-
-    slideImg.style.height = 'calc(100% - ' + textHeight + 'px)'
-
     slideshowContainer.appendChild(slideDiv)
 
     //TODO: add alt-text to gifs
@@ -96,7 +56,7 @@ function displaySlide(n) {
     slideDiv.style.display = "none";
   })
 
-  slideDivs[slideIndex].style.display = "inline-block"
+  slideDivs[slideIndex].style.display = "flex"
 }
 
 createSlides()
