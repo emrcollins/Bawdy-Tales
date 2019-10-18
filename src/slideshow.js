8401a43d84fa2cc75d36c180eed6a825eb2b8f34
Mariana Templin <mariana.acuna.templin@gmail.com>
2019-10-18 10:10:31 -0400

    add audio controls

--- src/slideshow.js
+++ src/slideshow.js
@@ -71,13 +71,84 @@ function displaySlide(n) {
   if(slide.audio){
     let slideAudio = document.createElement('audio')
     slideAudio.setAttribute('autoplay', 'autoplay')
-    slideAudio.setAttribute('controls', 'controls')
+    slideAudio.setAttribute('id', 'player')
+    // slideAudio.setAttribute('controls', 'controls')
+
+    let playButton = document.createElement('button')
+    playButton.style.display = 'none'
+    // let playSpan = document.createElement('span')
+    playButton.setAttribute('name', 'play sound')
+    playButton.setAttribute('id', 'play')
+    playButton.innerHTML = '&#9654;'
+    // playButton.appendChild(playSpan)
+
+    playButton.addEventListener('click', event => {
+      document.getElementById('player').play()
+      playButton.style.display = 'none'
+      document.getElementById('pause').style.display ='inline'
+    })
+
+
+    let pauseButton = document.createElement('button')
+    pauseButton.setAttribute('name', 'pause sound')
+    pauseButton.setAttribute('id', 'pause')
+    pauseButton.innerHTML = '&#10073;&#10073;'
+    pauseButton.addEventListener('click', event => {
+      document.getElementById('player').pause()
+      pauseButton.style.display = 'none'
+      document.getElementById('play').style.display ='inline'
+    })
+
+    let decreaseVolumeButton = document.createElement('button')
+    decreaseVolumeButton.setAttribute('name', 'decrease volume')
+    decreaseVolumeButton.innerHTML = '&#8722'
+    decreaseVolumeButton.addEventListener('click', event => {
+      document.getElementById('player').volume -= 0.1
+    })
+
+    let increaseVoluemButton = document.createElement('button')
+    increaseVoluemButton.setAttribute('name', 'increase volume')
+    increaseVoluemButton.innerHTML = '&#43;'
+    increaseVoluemButton.addEventListener('click', event => {
+      document.getElementById('player').volume += 0.1
+    })
+
+
+    let muteButton = document.createElement('button')
+    muteButton.setAttribute('name', 'mute sound')
+    muteButton.setAttribute('id', 'mute')
+    muteButton.innerHTML = '&#x1f50a;&#xFE0E;'
+    muteButton.addEventListener('click', event => {
+      document.getElementById('player').muted = true
+      muteButton.style.display = 'none'
+      document.getElementById('unmute').style.display ='inline'
+    })
+
+    let unmuteButton = document.createElement('button')
+    unmuteButton.style.display = 'none'
+    unmuteButton.setAttribute('name', 'unmute sound')
+    unmuteButton.setAttribute('id', 'unmute')
+    unmuteButton.innerHTML = '&#128266;'
+    unmuteButton.addEventListener('click', event => {
+      document.getElementById('player').muted = false
+      unmuteButton.style.display = 'none'
+      document.getElementById('mute').style.display ='inline'
+    })
+
+    let audioControls = document.createElement('div')
+    audioControls.setAttribute('class', 'audioControls')
+    audioControls.appendChild(playButton)
+    audioControls.appendChild(pauseButton)
+    audioControls.appendChild(decreaseVolumeButton)
+    audioControls.appendChild(increaseVoluemButton)
+    audioControls.appendChild(muteButton)
 
     let audioSource = document.createElement('source')
     audioSource.setAttribute('src', data.path + 'audio/' + slide.audio)
 
     slideAudio.appendChild(audioSource);
     slideDivs[slideIndex].appendChild(slideAudio);
+    slideDivs[slideIndex].appendChild(audioControls)
   }
 
 }
